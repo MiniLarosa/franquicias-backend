e6f036b9be0b36cfcf2ca276ee29cc39cc958c9c
Sebastian Insignares <sebastianj-insignaresc@unilibre.edu.co>
2026-04-12 18:03:33 -0500

    feat: configuración inicial del backend reactivo de franquicias

---
+++ src/test/java/com/franquicias/backend/FranquiciasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franquicias.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
